9071a94dda1bdc84b52b7564762011f60f37dfee
flaviane-lima <rflaviane41@gmail.com>
2025-02-27 16:12:04 -0300

    modifiquei o nome da classe

--- operadores/src/OperadoresRelacionais.java
+++ operadores/src/OperadoresRelacionais.java
@@ -1,54 +1,56 @@
 public class OperadoresRelacionais {
 
-    public static void main(String[] args) {
-        // Os operadores relacionais avaliam a relação entre duas variáveis ou expressões
 
-        int numero1 = 1;
-        int numero2 = 2;
+        public static void main(String[] args) {
+            // Os operadores relacionais avaliam a relação entre duas variáveis ou expressões
 
-        boolean simNao = numero1 == numero2;
+            int numero1 = 1;
+            int numero2 = 2;
 
-        System.out.println("numero1 é igual a numero2: " + simNao);
+            boolean simNao = numero1 == numero2;
 
-        simNao = numero1 != numero2;
+            System.out.println("numero1 é igual a numero2: " + simNao);
 
-        System.out.println("numero1 é diferente a numero2: " + simNao);
+            simNao = numero1 != numero2;
 
-        simNao = numero1 > numero2;
+            System.out.println("numero1 é diferente a numero2: " + simNao);
 
-        System.out.println("numero1 é maior que numero2: " + simNao);
+            simNao = numero1 > numero2;
 
-        simNao = numero1 >= numero2;
-        System.out.println("numero1 é igual ou maior numero2: " + simNao);
+            System.out.println("numero1 é maior que numero2: " + simNao);
 
-        simNao = numero1 < numero2;
-        System.out.println("numero1 é menor que numero2: " + simNao);
+            simNao = numero1 >= numero2;
+            System.out.println("numero1 é igual ou maior numero2: " + simNao);
 
-        simNao = numero1 <= numero2;
-        System.out.println("numero1 é igual ou menor que o numero2: " + simNao);
+            simNao = numero1 < numero2;
+            System.out.println("numero1 é menor que numero2: " + simNao);
 
-        // EXEMPLO USANDO CONDICIONAIS
+            simNao = numero1 <= numero2;
+            System.out.println("numero1 é igual ou menor que o numero2: " + simNao);
 
-        if(numero1 == numero2) {
-            System.out.print("Numero 1 maior que numero 2");
+            // EXEMPLO USANDO CONDICIONAIS
+
+            if(numero1 == numero2) {
+                System.out.print("Numero 1 maior que numero 2");
+            }
+
+            if( numero1 != numero2) {
+                System.out.print("Numero 1 é diferente de numero 2");
+            }
+
+            if(numero1 > numero2)
+                System.out.print("Numero 1 maior que numero 2");
+
+            if(numero1 < numero2)
+                System.out.print("Numero 1 menor que numero 2");
+
+            if(numero1 >= numero2)
+                System.out.print("Numero 1 maior ou igual que numero 2");
+
+            if(numero1 <= numero2)
+                System.out.print("Numero 1 menor ou igual que numero 2");
+
+
         }
-
-        if( numero1 != numero2) {
-            System.out.print("Numero 1 é diferente de numero 2");
-        }
-
-        if(numero1 > numero2)
-            System.out.print("Numero 1 maior que numero 2");
-
-        if(numero1 < numero2)
-            System.out.print("Numero 1 menor que numero 2");
-
-        if(numero1 >= numero2)
-            System.out.print("Numero 1 maior ou igual que numero 2");
-
-        if(numero1 <= numero2)
-            System.out.print("Numero 1 menor ou igual que numero 2");
-
-
     }
-}
+
