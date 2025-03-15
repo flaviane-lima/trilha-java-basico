13dcf117c8eeb49129bf03099b680b80d10dae13
flaviane-lima <rflaviane41@gmail.com>
2025-03-15 17:33:13 -0300

    fixando conhecimento de condições ternárias

--- estruturas-condicionais/src/CondicionaisTernarias/ResultadoEscolar.java
+++ estruturas-condicionais/src/CondicionaisTernarias/ResultadoEscolar.java
@@ -1,4 +1,13 @@
 package CondicionaisTernarias;
 
 public class ResultadoEscolar {
+
+    public static void main(String[] args) {
+        int nota = 4;
+
+        // colocando em prática uso de ternário
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "recuperação" : "reprovado";
+        System.out.println(resultado);
+
+    }
 }
