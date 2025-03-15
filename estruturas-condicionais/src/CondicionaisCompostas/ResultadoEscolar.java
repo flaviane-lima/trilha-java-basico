59baf7e90a684c9021b21d109957daa5cb30b5ae
flaviane-lima <rflaviane41@gmail.com>
2025-03-15 16:44:38 -0300

    praticando estrutura condicional compotsa

--- estruturas-condicionais/src/CondicionaisCompostas/ResultadoEscolar.java
+++ estruturas-condicionais/src/CondicionaisCompostas/ResultadoEscolar.java
@@ -1,4 +1,16 @@
 package CondicionaisCompostas;
 
 public class ResultadoEscolar {
+
+    public static void main(String[] args) {
+
+        int nota = 6;
+
+        if ( nota >= 7 ) {
+            System.out.println("Aprovado");
+        } else {
+            System.out.println("Reprovado");
+        }
+
+    }
 }
