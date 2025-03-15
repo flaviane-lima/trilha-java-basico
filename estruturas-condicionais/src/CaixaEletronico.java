943b175b0ac88faf8e98e7958001c9344aa6d48d
flaviane-lima <rflaviane41@gmail.com>
2025-03-15 12:40:59 -0300

    criação da classe CaixaEletronico, para praticar a logica de estrutura condicional simples

---
+++ estruturas-condicionais/src/CaixaEletronico.java
@@ -0,0 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CaixaEletronico {
+    public static void main(String[] args) {
+
+        // lendo os dados digitados pelo usuário
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o valor que quer digitar: ");
+        double valorSolicitado = scanner.nextDouble();
+
+        // saldo que tem na conta
+        double saldo = 25.0;
+
+        // implementando a estrutura condiciona simples
+        if (valorSolicitado < saldo) {
+
+            saldo = saldo - valorSolicitado;
+
+        }
+
+        // printando na tela a saída
+        System.out.printf("Saldo atual: %.2f%n", saldo);
+
+
+
+    }
+}
