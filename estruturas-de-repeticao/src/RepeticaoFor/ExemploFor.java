06a2f2902c2b8443fc1d0f66200b1414dc3911c1
flaviane-lima <rflaviane41@gmail.com>
2025-03-16 22:07:01 -0300

    exemplo de for usando duas estruturas

---
+++ estruturas-de-repeticao/src/RepeticaoFor/ExemploFor.java
@@ -0,0 +1,25 @@
+package RepeticaoFor;
+
+public class ExemploFor {
+
+    public static void main(String[] args) {
+
+        /*
+        * umas das maneira de se fazer o for
+        int carneirinhos = 1;
+        for ( ; carneirinhos <= 20; ) {
+            System.out.println(carneirinhos + " - Carneirinhos");
+            carneirinhos ++;
+
+        }
+
+         */
+
+        // outra estrutura
+        for (int carneirinhos = 1 ; carneirinhos <= 20 ; carneirinhos ++) {
+            System.out.println(carneirinhos + " - Carneirinhos ");
+        }
+        System.out.println("João Rafael dormiu!");
+
+    }
+}
