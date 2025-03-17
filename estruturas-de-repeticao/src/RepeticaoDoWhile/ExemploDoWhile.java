aa42d664934d977d6d39f813feca501c2898b5c9
flaviane-lima <rflaviane41@gmail.com>
2025-03-17 08:28:06 -0300

    Logica aplicada para do while

--- estruturas-de-repeticao/src/RepeticaoDoWhile/ExemploDoWhile.java
+++ estruturas-de-repeticao/src/RepeticaoDoWhile/ExemploDoWhile.java
@@ -1,4 +1,29 @@
 package RepeticaoDoWhile;
 
+import java.util.Random;
+
 public class ExemploDoWhile {
+
+    public static void main(String[] args) {
+
+        System.out.println("Discando...");
+
+        // estrutura do while
+        do {
+            // vai executar até a pessoa atender
+            System.out.println("Telefone tocando");
+        } while (tocando());
+        System.out.println("Alô !!!");
+
+    }
+
+    // método
+    private static boolean tocando() {
+        boolean atendeu = new Random().nextInt(3) == 1;
+        System.out.println("Atendeu? " + atendeu);
+
+        // negando o ato de continuar tocando
+        return !atendeu;
+
+    }
 }
