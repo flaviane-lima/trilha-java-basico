96da0aad609ab3844ad394ed06fb171f35f5b52f
flaviane-lima <rflaviane41@gmail.com>
2025-03-17 00:00:16 -0300

    Exemplo de código com While

--- estruturas-de-repeticao/src/RepeticaoWhile/ExemploWhile.java
+++ estruturas-de-repeticao/src/RepeticaoWhile/ExemploWhile.java
@@ -1,4 +1,32 @@
 package RepeticaoWhile;
+import java.util.concurrent.ThreadLocalRandom;
+
 
 public class ExemploWhile {
+
+    public static void main(String[] args) {
+
+        double mesada = 50.0;
+            while ( mesada > 0) {
+                double valorDoce = valorAleatorio();
+                if (valorDoce > mesada) {
+
+                    valorDoce = mesada;
+
+                }
+                System.out.println("Doce do valor: " + valorDoce + " Adicionado no carrinho");
+                mesada = mesada - valorDoce;
+
+                System.out.println("Mesada:" + mesada);
+                
+                System.out.println("João Rafael gastou toda a sua mesada em doce");
+            }
+
+
+
+    }
+    //criando o método
+    private static double valorAleatorio() {
+        return ThreadLocalRandom.current().nextDouble(2,8);
+    }
 }
